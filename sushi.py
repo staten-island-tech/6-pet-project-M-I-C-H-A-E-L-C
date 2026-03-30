f84a2e8e51dc4625c2e6803191aa58c04c26b2b3
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-03-30 10:18:52 -0400

    .

---
+++ sushi.py
@@ -0,0 +1,19 @@
+sushi_orders = [
+    {"name": "California Roll", "price": 8},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8},
+    {"name": "Dragon Roll", "price": 12},
+    {"name": "Spicy Tuna Roll", "price": 10},
+    {"name": "Miso Soup", "price": 4},
+    {"name": "Edamame", "price": 5},
+    {"name": "Salmon Nigiri", "price": 6},
+    {"name": "California Roll", "price": 8}
+]
+def reciept(orders):
+    the_reciept = {}
+    for sushi in orders:
+        if sushi['name'] in the_reciept:
+            continue
+        else:
+            the_reciept[sushi['name']] = {
