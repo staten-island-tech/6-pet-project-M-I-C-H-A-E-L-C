a879e6f7c31332a152774111e1735cd9180cdc3e
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-04-14 10:17:30 -0400

    1

--- pet.py
+++ pet.py
@@ -1,3 +1,4 @@
+import random
 class Pet:
     def __init__(self, name, friendliness, hunger, bloodlust, thirst):
         self.name = name
@@ -5,15 +6,23 @@ class Pet:
         self.hunger = hunger
         self.bloodlust = bloodlust
         self.thirst = thirst
-
-MrBob = Pet("MrBob", 101, 90, 16, 35)
-if MrBob.name != MrBob:
-    print("Not MrBob")
-else:
-    print("Is MrBob")
+x = random.randint(1,10000)
+y = random.randint(1,200)
+MrBob = Pet('MrBob', y, 90, x, 35)
 if MrBob.friendliness > 100:
     print("MrBob heppy")
 else:
     print("MrBob angy")
 if MrBob.hunger > 100:
     print("MrBob Hungy!")
+else:
+    print("MrBob Satisfied")
+if MrBob.bloodlust > 9000:
+    print("MrBob sinister level over 9000! >:(")
+elif MrBob.bloodlust > 100:
+    print("MrBob sinister  >_<")
+else:
+    print("MrBob kind (: ")
+z = input("pet MrBob to increase friendliness? Yes - No")
+if z == "Yes":
+    y.append(MrBob.friendliness) 
