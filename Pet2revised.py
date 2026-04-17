bd7df483a7522bd91657e007a62326f6f66e283f
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-04-17 10:17:43 -0400

    !

--- Pet2revised.py
+++ Pet2revised.py
@@ -12,8 +12,32 @@ x = random.randint(1,10000) # Bloodlust
 v = random.randint(1,200) # Thirst
 MrBob = Pet('MrBob', y, w, x, v)
 print(MrBob.name)
-print("Friendliness:", y)
-print("Hunger:", w)
-print("Bloodlust:", x)
-print("Thirst:", v)
-z = input("Pet for friendliness, Feed for hunger, ")
+print("😄", y, "🍖", w, "🤬", x , "💧", v)
+if w >= 0 and v >= 0:
+ #   m = input("Give MrBob water? ")
+    z = input("Feed Mr.Bob? ").upper()
+    if z == "YES":
+        y += 10
+        w += 10
+        x -= 100
+        v -= 10
+        print("😄", y, "🍖", w, "🤬", x , "💧", v)
+    if z == "NO":
+        print("...")
+        print("😄", y, "🍖", w, "🤬", x , "💧", v)
+    if MrBob.hunger >= 100:
+        print("MrBob full")
+    n = input("Play with Mr.Bob? ").upper()
+    if n == "YES":
+        y += 10
+        w -= 10
+        x -= 100
+        v -= 10
+        print("😄", y, "🍖", w, "🤬", x , "💧", v)
+    if n == "NO":
+        print("...")
+        y -= 10
+        x += 50
+        print("😄", y, "🍖", w, "🤬", x , "💧", v)
+    if MrBob.friendliness >= 100:
+        print("MrBob happy")
