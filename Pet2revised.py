8d5590854e62d6e430a3bdc17ddfd5467eed3aac
M-I-C-H-A-E-L-C <michaelc1242@nycstudents.net>
2026-04-20 10:17:36 -0400

    1

--- Pet2revised.py
+++ Pet2revised.py
@@ -15,29 +15,41 @@ print(MrBob.name)
 print("😄", y, "🍖", w, "🤬", x , "💧", v)
 if w >= 0 and v >= 0:
  #   m = input("Give MrBob water? ")
-    z = input("Feed Mr.Bob? ").upper()
-    if z == "YES":
-        y += 10
-        w += 10
-        x -= 100
-        v -= 10
-        print("😄", y, "🍖", w, "🤬", x , "💧", v)
-    if z == "NO":
-        print("...")
-        print("😄", y, "🍖", w, "🤬", x , "💧", v)
-    if MrBob.hunger >= 100:
+    if MrBob.hunger <= 100:
+        z = input("Feed Mr.Bob? ").upper()
+        if z == "YES":
+            y += 10 # Friendliness
+            w += 10 # Hunger
+            x -= 100 # Bloodlust
+            v -= 10 # Thirst
+            print("😄🔺", y, "🍖🔺", w, "🤬🔻", x , "💧🔻", v)
+        elif z == "NO":
+            print("...")
+            y -= 10 # Friendliness
+            x += 50 # Bloodlust
+            print("😄🔻", y, "🍖", w, "🤬🔺", x , "💧", v)
+        else:
+            print("It's yes or no chud")
+            z = input("Feed Mr.Bob? ").upper()
+    elif MrBob.hunger >= 100:
         print("MrBob full")
+        print("😄", y, "🍖", w, "🤬", x , "💧", v)
     n = input("Play with Mr.Bob? ").upper()
     if n == "YES":
-        y += 10
-        w -= 10
-        x -= 100
-        v -= 10
-        print("😄", y, "🍖", w, "🤬", x , "💧", v)
-    if n == "NO":
+        y += 10 # Friendliness
+        w -= 10 # Hunger
+        x -= 100 # Bloodlust
+        v -= 10 # Thirst
+        print("😄🔺", y, "🍖🔻", w, "🤬🔻", x , "💧🔻", v)
+    elif n == "NO":
         print("...")
-        y -= 10
-        x += 50
-        print("😄", y, "🍖", w, "🤬", x , "💧", v)
+        y -= 10 # Friendliness
+        x += 50 # Bloodlust
+        print("😄🔻", y, "🍖", w, "🤬🔺", x , "💧", v)
+    else:
+        print("It's yes or no chud")
+        n = input("Play with Mr.Bob? ").upper()
     if MrBob.friendliness >= 100:
         print("MrBob happy")
+elif w < 0 or v < 0:
+    print("MrBob ded")
